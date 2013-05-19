b73bcb40c0e7cf411fefa6c8650cd6a4153f944a
Philip Barton <audiophyl@gmail.com>
2013-05-19 00:42:43 -0500

    Initial commit.

---
+++ makepdf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# set up our vars
+OUTPUT_DIR="output"
+TOP_DIR=`pwd`
+
+# make our output directory (no worries if this already exists)
+mkdir $OUTPUT_DIR
+
+# clean up any past output
+cd $OUTPUT_DIR
+rm *.aux *.log *.toc
+cd $TOP_DIR
+
+# do this twice to ensure proper ToC -- this is NORMAL for complex LaTeX documents
+pdflatex -output-directory=$OUTPUT_DIR code.tex
+pdflatex -output-directory=$OUTPUT_DIR code.tex
+
