54acbc11581e38d9595d95d2aac89c206df5a124
Philip Barton <audiophyl@gmail.com>
2013-05-25 02:50:19 -0500

    sensible changes to makepdf.sh

--- makepdf.sh
+++ makepdf.sh
@@ -13,6 +13,8 @@ rm *.aux *.log *.toc *.pdf *.html *.css *.pl *.maf *.mtc *.mtc* WARNINGS
 cd $TOP_DIR
 
 # do this twice to ensure proper ToC -- this is NORMAL for complex LaTeX documents
-pdflatex -output-directory=$OUTPUT_DIR code.tex
-pdflatex -output-directory=$OUTPUT_DIR code.tex
+# don't write the pdf the first time (just the support files), halt on any errors,
+# and only print output from a single execution
+pdflatex -draftmode -halt-on-error -output-directory=$OUTPUT_DIR code.tex > /dev/null
+pdflatex -halt-on-error -output-directory=$OUTPUT_DIR code.tex
 
